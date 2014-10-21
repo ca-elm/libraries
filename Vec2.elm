9c420a5269a52d240c0709fe65158036a6935ab1
Nathan Dinsmore <nfdins@gmail.com>
2014-10-21 01:12:36 -0400

    Remove a lot of twos
    The new names fit better with Elm's conventions.

--- Vec2.elm
+++ Vec2.elm
@@ -7,7 +7,7 @@ tuples of their x and y components, so you can use `(,)` as a constructor.
 @docs Vec2
 
 # Constant Vectors
-@docs zero2, one2, right2, left2, up2, down2
+@docs zero, one, right, left, up, down
 
 # Operators
 @docs (@+), (@-), (@*), (@/), (@.)
@@ -18,13 +18,13 @@ Compare two vectors by length.
 @docs (@<), (@<=), (@=), (@>), (@>=)
 
 # Scaling
-@docs unit2, scale2, scalei2, withlen2, neg2, inv2
+@docs normalize, scale, scaleInverse, withLength, invert, reciprocate
 
 # Transforming
-@docs rotl2, rotr2, rot2, fliph2, flipv2, map2
+@docs rotateLeft, rotateRight, rotate, flipX, flipY, fmap
 
 # Accessing
-@docs theta2, len2, lensqr2, angle2
+@docs theta, length, lengthSquared, angle
 
 # Drawing
 @docs arrow
@@ -35,51 +35,51 @@ Compare two vectors by length.
 type Vec2 = (Float, Float)
 
 {-| The zero vector. -}
-zero2 : Vec2
-zero2 = (0,0)
+zero : Vec2
+zero = (0,0)
 {-| A vector of ones: `(1,1)`. -}
-one2 : Vec2
-one2 = (1,1)
+one : Vec2
+one = (1,1)
 {-| A unit vector pointing to the right. -}
-right2 : Vec2
-right2 = (1,0)
+right : Vec2
+right = (1,0)
 {-| A unit vector pointing to the left. -}
-left2 : Vec2
-left2 = (-1,0)
+left : Vec2
+left = (-1,0)
 {-| A unit vector pointing up. -}
-up2 : Vec2
-up2 = (0,1)
+up : Vec2
+up = (0,1)
 {-| A unit vector pointing down. -}
-down2 : Vec2
-down2 = (0,-1)
+down : Vec2
+down = (0,-1)
 
 {-| Add the components of two vectors.
 
       (1,2) @+ (3,4) == (4,6)
 -}
 (@+) : Vec2 -> Vec2 -> Vec2
-(@+) = bin2 (+)
+(@+) = binary (+)
 
 {-| Subtract the components of two vectors.
 
       (5,7) @- (3,4) == (2,3)
 -}
 (@-) : Vec2 -> Vec2 -> Vec2
-(@-) = bin2 (-)
+(@-) = binary (-)
 
 {-| Multiply the components of two vectors.
 
       (3,2) @* (3,4) == (9,8)
 -}
 (@*) : Vec2 -> Vec2 -> Vec2
-(@*) = bin2 (*)
+(@*) = binary (*)
 
 {-| Divide the components of two vectors.
 
       (6,8) @/ (3,2) == (2,4)
 -}
 (@/) : Vec2 -> Vec2 -> Vec2
-(@/) = bin2 (/)
+(@/) = binary (/)
 
 {-| Compute the dot product of two vectors.
 
@@ -88,145 +88,145 @@ down2 = (0,-1)
 (@.) : Vec2 -> Vec2 -> Float
 (x,y) @. (x',y') = x*x' + y*y'
 
-bin2 f (x,y) (x',y') = (f x x', f y y')
+binary f (x,y) (x',y') = (f x x', f y y')
 
 (@<) : Vec2 -> Vec2 -> Bool
-(@<) = compare2 (<)
+(@<) = compareWith (<)
 
 (@<=) : Vec2 -> Vec2 -> Bool
-(@<=) = compare2 (<=)
+(@<=) = compareWith (<=)
 
 (@=) : Vec2 -> Vec2 -> Bool
-(@=) = compare2 (==)
+(@=) = compareWith (==)
 
 (@>=) : Vec2 -> Vec2 -> Bool
-(@>=) = compare2 (>=)
+(@>=) = compareWith (>=)
 
 (@>) : Vec2 -> Vec2 -> Bool
-(@>) = compare2 (>)
+(@>) = compareWith (>)
 
-compare2 f u v = lensqr2 u `f` lensqr2 v
+compareWith f u v = lengthSquared u `f` lengthSquared v
 
 {-| Normalize a vector, i.e., compute a vector in the same direction with length 1.
 
-      unit2 (3,4) == (0.6,0.8)
-      unit2 x == x `withlen2` 1
+      normalize (3,4) == (0.6,0.8)
+      normalize x == x `withLength` 1
 -}
-unit2 : Vec2 -> Vec2
-unit2 v = scalei2 (len2 v) v
+normalize : Vec2 -> Vec2
+normalize v = scaleInverse (length v) v
 
 {-| Scale the components of a vector by a scalar factor.
 
-      scale2 3 (1,2) == (3,6)
+      scale 3 (1,2) == (3,6)
 -}
-scale2 : Float -> Vec2 -> Vec2
-scale2 u (x,y) = (x*u, y*u)
+scale : Float -> Vec2 -> Vec2
+scale u (x,y) = (x*u, y*u)
 
 {-| Scale the components of a vector by the reciprocal of a scalar factor.
 
-      scalei2 3 (3,6) == (1,2)
-      scale2 x . scalei2 x == id
+      scaleInverse 3 (3,6) == (1,2)
+      scale x . scaleInverse x == id
 -}
-scalei2 : Float -> Vec2 -> Vec2
-scalei2 u (x, y) = (x/u, y/u)
+scaleInverse : Float -> Vec2 -> Vec2
+scaleInverse u (x, y) = (x/u, y/u)
 
 {-| Scale a vector so it has the given length. Usually used as a binary operator,
-e.g. ``(3,4) `withlen2` 10``.
+e.g. ``(2,3) `withLength` 5``.
 
-      (3,4) `withlen2` 10 == (6,8)
+      (3,4) `withLength` 10 == (6,8)
 -}
-withlen2 : Vec2 -> Float -> Vec2
-withlen2 v l = unit2 v |> scale2 l
+withLength : Vec2 -> Float -> Vec2
+withLength v l = normalize v |> scale l
 
 {-| Computes the square of the length of a vector. This is faster and can be
-used to compare lengths, e.g., `lensqr2 (3,4) < lensqr2 (2, 5)`.
+used to compare lengths, e.g., `lengthSquared (3,4) < lengthSquared (2, 5)`.
 
-      lensqr2 (1,4) == 17
+      lengthSquared (1,4) == 17
 -}
-lensqr2 : Vec2 -> Float
-lensqr2 u = u @. u
+lengthSquared : Vec2 -> Float
+lengthSquared u = u @. u
 
 {-| Computes the square of the length of a vector.
 
-      len2 (3,4) == 5
+      length (3,4) == 5
 -}
-len2 : Vec2 -> Float
-len2 = sqrt << lensqr2
+length : Vec2 -> Float
+length = sqrt << lengthSquared
 
 {-| Rotate a vector 90 degrees counterclockwise (to the left).
 
-      rotl2 (3,4) == (-4,3)
+      rotateLeft (3,4) == (-4,3)
 -}
-rotl2 : Vec2 -> Vec2
-rotl2 (x,y) = (-y,x)
+rotateLeft : Vec2 -> Vec2
+rotateLeft (x,y) = (-y,x)
 
 {-| Rotate a vector 90 degrees clockwise (to the right).
 
-      rotr2 (3,4) == (4,-3)
-      rotr2 == neg2 . rotl2
+      rotateRight (3,4) == (4,-3)
+      rotateRight == invert . rotateLeft
 -}
-rotr2 : Vec2 -> Vec2
-rotr2 (x,y) = (y,-x)
+rotateRight : Vec2 -> Vec2
+rotateRight (x,y) = (y,-x)
 
 {-| Rotate a vector 180 degrees, i.e., invert its components.
 
-      neg2 (5,2) == (-5,-2)
+      invert (5,2) == (-5,-2)
 -}
-neg2 : Vec2 -> Vec2
-neg2 (x,y) = (-x,-y)
+invert : Vec2 -> Vec2
+invert (x,y) = (-x,-y)
 
 {-| Compute the reciprocals of the components of a vector.
 
-      inv2 (2,4) == (0.5, 0.25)
+      reciprocate (2,4) == (0.5, 0.25)
 -}
-inv2 : Vec2 -> Vec2
-inv2 (x,y) = (1/x,1/y)
+reciprocate : Vec2 -> Vec2
+reciprocate (x,y) = (1/x,1/y)
 
 {-| Rotate a vector counterclockwise by the given angle.
 
-      rot2 (degrees 90) (3,4) == (-4,3)
-      rot2 (degrees 90) up2 == left2
+      rotate (degrees 90) (3,4) == (-4,3)
+      rotate (degrees 90) up2 == left2
 -}
-rot2 : Float -> Vec2 -> Vec2
-rot2 a (x,y) =
+rotate : Float -> Vec2 -> Vec2
+rotate a (x,y) =
   let (c,s) = (cos a,sin a) in
   (c*x - s*y, s*x + c*y)
 
 {-| Reflect a vector horizontally (across the y axis).
 
-      fliph2 (1,7) == (-1,7)
+      flipX (1,7) == (-1,7)
 -}
-fliph2 : Vec2 -> Vec2
-fliph2 (x,y) = (-x,y)
+flipX : Vec2 -> Vec2
+flipX (x,y) = (-x,y)
 
 {-| Reflect a vector vertically (across the x axis).
 
-      flipv2 (1,7) == (1,-7)
+      flipY (1,7) == (1,-7)
 -}
-flipv2 : Vec2 -> Vec2
-flipv2 (x,y) = (x,-y)
+flipY : Vec2 -> Vec2
+flipY (x,y) = (x,-y)
   
 {-| Map a function over the components of a vector.
 
-      map2 (\x -> x^2) (4,5) == (16,25)
+      fmap (\x -> x^2) (4,5) == (16,25)
 -}
-map2 : (Float -> Float) -> Vec2 -> Vec2
-map2 f (x,y) = (f x, f y)
+fmap : (Float -> Float) -> Vec2 -> Vec2
+fmap f (x,y) = (f x, f y)
 
 {-| Compute the angle between a vector and the positive x axis, i.e., the angle
 between the vector and `right2`.
 
-      theta2 (1,1) == degrees 45
+      theta (1,1) == degrees 45
 -}
-theta2 : Vec2 -> Float
-theta2 (x,y) = atan2 y x
+theta : Vec2 -> Float
+theta (x,y) = atan2 y x
 
 {-| Compute the smaller angle between two vectors.
 
-      angle2 up2 left2 == degrees 90
+      angle up2 left2 == degrees 90
 -}
-angle2 : Vec2 -> Vec2 -> Float
-angle2 u v = (u @. v) / len2 u / len2 v |> acos
+angle : Vec2 -> Vec2 -> Float
+angle u v = (u @. v) / length u / length v |> acos
 
 {-| Create a path which draws an arrow between two points.
 
@@ -235,7 +235,7 @@ angle2 u v = (u @. v) / len2 u / len2 v |> acos
 -}
 arrow : Float -> Vec2 -> Vec2 -> Path
 arrow r p q =
-  let d = p @- q |> unit2
-      e = r `scale2` rot2 (degrees -45) d
-      f = r `scale2` rot2 (degrees 45) d in
+  let d = p @- q |> normalize
+      e = scale r <| rotate (degrees -45) d
+      f = scale r <| rotate (degrees 45) d in
   path [p, q, q @+ e, q, q @+ f]
