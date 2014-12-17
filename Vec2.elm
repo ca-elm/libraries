79d58bc1d8604ebba7492dbe1c6a08197a174cad
Nathan Dinsmore <nfdins@gmail.com>
2014-12-17 18:34:15 -0500

    Fix documentation indentation

--- Vec2.elm
+++ Vec2.elm
@@ -66,22 +66,22 @@ down = (0,-1)
 
 {-| Compute the componentwise sum of two vectors.
 
-      sum (1,2) (3,4) == (4,6)
+    sum (1,2) (3,4) == (4,6)
 -}
 sum : Vec2 -> Vec2 -> Vec2
 sum = binary (+)
 
 {-| Compute the componentwise difference between two vectors.
 
-      difference (5,7) (3,4) == (2,3)
+    difference (5,7) (3,4) == (2,3)
 -}
 difference : Vec2 -> Vec2 -> Vec2
 difference = binary (-)
 
 {-| Subtract the components of a vector from those of another.
 
-      subtract (3,4) (5,7) == (2,3)
-      map (subtract one) [(3,4), (1,5)] == [(2,3), (0,4)]
+    subtract (3,4) (5,7) == (2,3)
+    map (subtract one) [(3,4), (1,5)] == [(2,3), (0,4)]
 
 Note that `subtract` is the same thing as `flip difference`
 -}
@@ -90,14 +90,14 @@ subtract = flip difference
 
 {-| Compute the componentwise product of two vectors.
 
-      product (3,2) (3,4) == (9,8)
+    product (3,2) (3,4) == (9,8)
 -}
 product : Vec2 -> Vec2 -> Vec2
 product = binary (*)
 
 {-| Compute the componentwise quotient of two vectors.
 
-      quotient (6,8) (3,2) == (2,4)
+    quotient (6,8) (3,2) == (2,4)
 -}
 quotient : Vec2 -> Vec2 -> Vec2
 quotient = binary (/)
@@ -106,29 +106,29 @@ binary f (x,y) (x',y') = (f x x', f y y')
 
 {-| Compute the dot product of two vectors.
 
-      dot (2,3) (4,5) == 23
+    dot (2,3) (4,5) == 23
 -}
 dot : Vec2 -> Vec2 -> Float
 dot (x,y) (x',y') = x*x' + y*y'
 
 {-| Normalize a vector, i.e., compute a vector in the same direction with length 1.
 
-      normalize (3,4) == (0.6,0.8)
-      normalize x == x `withLength` 1
+    normalize (3,4) == (0.6,0.8)
+    normalize x == x `withLength` 1
 -}
 normalize : Vec2 -> Vec2
 normalize v = scaleInverse (length v) v
 
 {-| Scale the components of a vector by a scalar factor.
 
-      scale 3 (1,2) == (3,6)
+    scale 3 (1,2) == (3,6)
 -}
 scale : Float -> Vec2 -> Vec2
 scale u (x,y) = (x*u, y*u)
 
 {-| Scale the components of a vector by the reciprocal of a scalar factor.
 
-      scaleInverse 3 (3,6) == (1,2)
+    scaleInverse 3 (3,6) == (1,2)
 -}
 scaleInverse : Float -> Vec2 -> Vec2
 scaleInverse u (x, y) = (x/u, y/u)
@@ -136,7 +136,7 @@ scaleInverse u (x, y) = (x/u, y/u)
 {-| Scale a vector so it has the given length. Usually used as a binary operator,
 e.g. ``(2,3) `withLength` 5``.
 
-      (3,4) `withLength` 10 == (6,8)
+    (3,4) `withLength` 10 == (6,8)
 -}
 withLength : Vec2 -> Float -> Vec2
 withLength v l = normalize v |> scale l
@@ -145,21 +145,21 @@ withLength v l = normalize v |> scale l
 computing the length and can still be used to compare lengths, e.g.,
 `lengthSquared (3,4) < lengthSquared (2, 5)`.
 
-      lengthSquared (1,4) == 17
+    lengthSquared (1,4) == 17
 -}
 lengthSquared : Vec2 -> Float
 lengthSquared u = dot u u
 
 {-| Computes the square of the length of a vector.
 
-      length (3,4) == 5
+    length (3,4) == 5
 -}
 length : Vec2 -> Float
 length = sqrt << lengthSquared
 
 {-| Rotate a vector 90 degrees counterclockwise (to the left).
 
-      rotateLeft (3,4) == (-4,3)
+    rotateLeft (3,4) == (-4,3)
 
 Note that `rotateLeft` is the same thing as `invert << rotateRight`.
 -}
@@ -168,7 +168,7 @@ rotateLeft (x,y) = (-y,x)
 
 {-| Rotate a vector 90 degrees clockwise (to the right).
 
-      rotateRight (3,4) == (4,-3)
+    rotateRight (3,4) == (4,-3)
 
 Note that `rotateRight` is the same thing as `invert << rotateLeft`.
 -}
@@ -177,22 +177,22 @@ rotateRight (x,y) = (y,-x)
 
 {-| Rotate a vector 180 degrees, i.e., invert its components.
 
-      invert (5,2) == (-5,-2)
+    invert (5,2) == (-5,-2)
 -}
 invert : Vec2 -> Vec2
 invert (x,y) = (-x,-y)
 
 {-| Compute the reciprocals of the components of a vector.
 
-      reciprocate (2,4) == (0.5, 0.25)
+    reciprocate (2,4) == (0.5, 0.25)
 -}
 reciprocate : Vec2 -> Vec2
 reciprocate (x,y) = (1/x,1/y)
 
 {-| Rotate a vector counterclockwise by the given angle.
 
-      rotate (degrees 90) (3,4) == (-4,3)
-      rotate (degrees 90) up == left
+    rotate (degrees 90) (3,4) == (-4,3)
+    rotate (degrees 90) up == left
 -}
 rotate : Float -> Vec2 -> Vec2
 rotate a (x,y) =
@@ -201,35 +201,35 @@ rotate a (x,y) =
 
 {-| Rotate a vector counterclockwise around the given center by the given angle.
 
-      rotateAround (1,1) (degrees 90) (3,4) == (-2,3)
+    rotateAround (1,1) (degrees 90) (3,4) == (-2,3)
 -}
 rotateAround : Vec2 -> Float -> Vec2 -> Vec2
 rotateAround c a = sum c << rotate a << subtract c
 
 {-| Reflect a vector horizontally (across the y axis).
 
-      flipX (1,7) == (-1,7)
+    flipX (1,7) == (-1,7)
 -}
 flipX : Vec2 -> Vec2
 flipX (x,y) = (-x,y)
 
 {-| Reflect a vector vertically (across the x axis).
 
-      flipY (1,7) == (1,-7)
+    flipY (1,7) == (1,-7)
 -}
 flipY : Vec2 -> Vec2
 flipY (x,y) = (x,-y)
 
 {-| Swap the components of a vector.
 
-      transpose (4,7) == (7,4)
+    transpose (4,7) == (7,4)
 -}
 transpose : Vec2 -> Vec2
 transpose (x,y) = (y,x)
 
 {-| Map a function over the components of a vector.
 
-      map (\x -> x^2) (4,5) == (16,25)
+    map (\x -> x^2) (4,5) == (16,25)
 -}
 map : (Float -> Float) -> Vec2 -> Vec2
 map f (x,y) = (f x, f y)
@@ -237,14 +237,14 @@ map f (x,y) = (f x, f y)
 {-| Compute the angle between a vector and the positive x axis, i.e., the angle
 between the vector and `right`.
 
-      theta (1,1) == degrees 45
+    theta (1,1) == degrees 45
 -}
 theta : Vec2 -> Float
 theta (x,y) = atan2 y x
 
 {-| Compute the distance between two vectors.
 
-      V.distance (1,2) (-2,-2) == 5
+    V.distance (1,2) (-2,-2) == 5
 -}
 distance : Vec2 -> Vec2 -> Float
 distance a = length << difference a
@@ -252,43 +252,43 @@ distance a = length << difference a
 {-| Compute the square of the distance between two vectors. This is faster than
 computing the distance and can still be used to compare distances.
 
-      V.distanceSquared (1,2) (5,3) == 17
+    V.distanceSquared (1,2) (5,3) == 17
 -}
 distanceSquared : Vec2 -> Vec2 -> Float
 distanceSquared a = lengthSquared << difference a
 
 {-| Compute the smaller angle between two vectors.
 
-      angle up left == degrees 90
+    angle up left == degrees 90
 -}
 angle : Vec2 -> Vec2 -> Float
 angle u v = dot u v / length u / length v |> acos
 
 {-| Compute the componentwise minimum of two vectors
 
-      min (2,5) (3,1) == (2,1)
+    min (2,5) (3,1) == (2,1)
 -}
 min : Vec2 -> Vec2 -> Vec2
 min (x,y) (x',y') = (Basics.min x x', Basics.min y y')
 
 {-| Compute the componentwise maximum of two vectors
 
-      max (2,5) (3,1) == (3,5)
+    max (2,5) (3,1) == (3,5)
 -}
 max : Vec2 -> Vec2 -> Vec2
 max (x,y) (x',y') = (Basics.max x x', Basics.max y y')
 
 {-| Convert a vector of ints to a Vec2
 
-      lift fromInts Mouse.position
+    lift fromInts Mouse.position
 -}
 fromInts : (Int,Int) -> Vec2
 fromInts (x,y) = (toFloat x, toFloat y)
 
 {-| Create a path which draws an arrow between two points.
 
-      collage 400 400 [
-        arrow 10 (0,0) (100,40) |> traced (solid blue)]
+    collage 400 400 [
+      arrow 10 (0,0) (100,40) |> traced (solid blue)]
 -}
 arrow : Float -> Vec2 -> Vec2 -> Path
 arrow r p q =
